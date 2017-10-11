6e2ccdf1320bf63c0067b222fd4213306522933c
Christopher Kellner <chr.kellner@live.de>
2017-10-11 20:12:57 +0200

    Simple test for $mdSidenav - added @types/angular-material - extended the app.component

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,21 +2,25 @@ interface IAppComponentBindings {
     username: string;
 }
 
-interface IAppController extends IAppComponentBindings, ng.IController
-{
-  do(): void;
+interface IAppController extends IAppComponentBindings, ng.IController {
+    do(): void;
+    toggle(): void;
 }
 
 export class AppController implements IAppController {
-  static $inject = ['$state'];
+    static $inject = ['$state', '$mdSidenav'];
 
-  public username: string = "Tom";
+    public username: string = "Tom";
 
-  public do(): void {
-    alert("test");
-  }
+    public toggle(): void {
+        this.$mdSidenav('menu-left')
+        .toggle();
+    }
 
-  constructor(private $state: ng.ui.IStateService) {
-    
-  }
+    public do(): void {
+        alert("test");
+    }
+
+    constructor(private $state: ng.ui.IStateService, private $mdSidenav: ng.material.ISidenavService) {
+    }
 }
