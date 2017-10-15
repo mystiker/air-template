01da073ff79cd6a760fefe82cb1bbbac2ea0bd2d
Christopher Kellner <chr.kellner@live.de>
2017-10-16 00:46:03 +0200

    Changed webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         host: '0.0.0.0'
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        //new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             template: 'src/index.template.ejs'
         }),
